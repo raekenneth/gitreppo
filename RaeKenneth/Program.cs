dc4f1f2c8360fa79abc8e8bdfeb0fe0c61392014
raekenneth <rkenneth@student.ciputra.ac.id>
2021-12-17 09:58:58 +0700

    Initial check-in of module RaeKenneth

---
+++ RaeKenneth/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RaeKenneth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string namaKu = "";
+            string hobbyKu = "";
+            string tanggallahir = "";
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
